a7a4c4498c6b29f9a49908ade74d451be6bfca3d
farooq-azam-khan <khanazamfarooq99@gmail.com>
2024-06-25 20:53:41 -0400

    manual gradients

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ class Value:
         self._prev = set(_children)
         self._op = _op
         self.label = label
+        self.grad = 0.0
 
     def __repr__(self):
-        return f"Value({self.data})"
+        return f"Value({self.data:.4E})"
 
     def __add__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
         return Value(
             self.data + other.data,
             _children=(self, other),
@@ -17,9 +20,31 @@ class Value:
         )
 
     def __mul__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
         return Value(
             self.data * other.data,
             _children=(self, other),
             _op="*",
             label=f"(* {self.label} {other.label})",
+        )
+
+    def __truediv__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+        return Value(
+            self.data / other.data,
+            _children=(self, other),
+            _op="/",
+            label=f"(/ {self.label} {other.label})",
+        )
+
+    def __sub__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+        return Value(
+            self.data - other.data,
+            _children=(self, other),
+            _op="-",
+            label=f"(- {self.label} {other.label})",
         )
